ab6401405b37b6f4f194c5bbe1555223d0ba20d5
Richard Fenix <richard.fenix@gmail.com>
2018-06-05 04:54:19 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,10 +13,8 @@ class School
   end
 
   def add_student(student, grade)
-    @student = student
-    @grade = grade
-    if @roster[:grade].nil?
-      @roster.[:grade] = []
+    if @roster[grade].nil?
+      @roster.[grade] = []
     end
     @roster[@grade] << student
   end
