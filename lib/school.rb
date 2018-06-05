1bab3f8c079fecd218fb93d1dfc8c237a99f8fc2
Richard Fenix <richard.fenix@gmail.com>
2018-06-05 04:32:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,7 +12,6 @@ class School
   def add_student(student, grade)
     @student = student
     @grade = grade
-    @roster[grade] = [] unless @roster[grade].length < 0
     @roster[grade] << student
   end
   
